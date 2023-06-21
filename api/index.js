196b30e2eadc99bfb32b0ff31b4e26f90cd6b8b2
AryanGupta1211 <124623548+AryanGupta1211@users.noreply.github.com>
2023-06-21 19:27:08 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -19,7 +19,7 @@ const secret = process.env.SECRET;
 
 const salt = bcrypt.genSaltSync(10);
 
-app.use(cors({credentials:true,origin:'https://steady-faloodeh-447b0f.netlify.app/'}));
+app.use(cors({credentials:true,origin:'http://localhost:3000'}));
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
